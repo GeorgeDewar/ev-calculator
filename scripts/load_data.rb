e3b93041bbb9f67884d777bd62935263a4970f05
George Dewar <george@dewar.co.nz>
2018-09-13 16:43:55 +1200

    Add pry, start work on reading XML file

--- scripts/load_data.rb
+++ scripts/load_data.rb
@@ -1,5 +1,6 @@
 require_relative '../config/environment'
 require 'optparse'
+require 'nokogiri'
 
 options = {}
 OptionParser.new do |opts|
@@ -14,3 +15,18 @@ if options[:filename].nil?
   puts "A filename is required. Try --help."
 end
 
+doc = File.open(options[:filename]) { |f| Nokogiri::XML(f) }
+doc.remove_namespaces! # The data comes with a single namespace
+
+doc.xpath("//brand").each do |brand|
+  brand.xpath("models/model").each do |model|
+    model.xpath("generations/generation").each do |generation|
+      start_year = generation.xpath("modifications/modification/yearstart").map(&:text).min
+      end_year = generation.xpath("modifications/modification/yearstop").map(&:text).max
+      puts "#{brand.xpath("name").text} #{model.xpath("name").text} (#{start_year}-#{end_year})"
+      generation.xpath("modifications/modification").each do |modification|
+        puts "  #{modification.xpath("engine").text}"
+      end
+    end
+  end
+end
