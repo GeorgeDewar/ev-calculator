4d6309dd4999fcb5168e3d7e20411e8500b62ae5
George Dewar <george@dewar.co.nz>
2018-09-16 21:53:52 +1200

    Start on React front end

---
+++ app/controllers/calculator_controller.rb
@@ -0,0 +1,6 @@
+class CalculatorController < ApplicationController
+  layout false
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :vehicles, only: [:show] do
     get :search, on: :collection
   end
+  root to: "calculator#index"
 end
