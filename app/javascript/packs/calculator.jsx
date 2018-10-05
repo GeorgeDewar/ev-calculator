cccb6971b64362d4587942ad7523211f3f0ebfea
George Dewar <george@dewar.co.nz>
2018-10-05 23:12:38 +1300

    Relayout, add lifespan

--- app/javascript/packs/calculator.jsx
+++ app/javascript/packs/calculator.jsx
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom';
 import VehicleSearch from '../components/VehicleSearch';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
+import { LIFESPAN } from '../constants';
 
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      vehicle_description: null,
-      kerb_weight: null,
-      fuel_type: null,
-      fuel_economy: null,
+      currentVehicle: {
+        description: null,
+        kerbWeight: null,
+        fuelType: null,
+        fuelEconomy: null,
+        value: null,
+      },
     };
     this.selectVehicle = this.selectVehicle.bind(this);
-  }
+  };
   selectVehicle(option) {
     console.log(option);
     fetch(`/vehicles/${option.id}.json`, {
@@ -23,10 +27,13 @@ export default class Calculator extends Component {
       .then(response => response.json())
       .then(response => {
         this.setState({
-          description: `${option.search_year} ${response.brand.name} ${response.model.name}`,
-          kerb_weight: response.kerb_weight,
-          fuel_type: response.fuel_type,
-          fuel_economy: response.fuel_consumption_combined,
+          currentVehicle: {
+            description: `${option.search_year} ${response.brand.name} ${response.model.name}`,
+            kerbWeight: response.kerb_weight,
+            fuelType: response.fuel_type,
+            fuelEconomy: response.fuel_consumption_combined || response.average_energy_consumption,
+            lifespan: LIFESPAN[response.fuel_type],
+          },
         });
         console.log(response);
       })
@@ -36,38 +43,99 @@ export default class Calculator extends Component {
   }
   handleChange = name => event => {
     this.setState({
-      [name]: event.target.value,
+      currentVehicle: {
+        ...this.state.currentVehicle,
+        [name]: event.target.value,
+      },
     });
   };
   render() {
+    var currentVehicle = this.state.currentVehicle;
     return (
-      <div>
-        <h2>Select your current vehicle</h2>
-        <VehicleSearch onChange={this.selectVehicle} />
-        <TextField
-          style={{ display: 'block' }} fullWidth
-          label="Vehicle Description"
-          value={this.state.description || ''}
-          onChange={this.handleChange('description')} />
-        <TextField
-          style={{ display: 'block' }} fullWidth type="number"
-          label="Kerb Weight (kg)"
-          value={this.state.kerb_weight || ''}
-          onChange={this.handleChange('kerb_weight')} />
-        <TextField
-          style={{ display: 'block' }} fullWidth select
-          label="Fuel Type" value={this.state.fuel_type || ''}
-          onChange={this.handleChange('fuel_type')}>
-          <MenuItem value="petrol">Petrol</MenuItem>
-          <MenuItem value="diesel">Diesel</MenuItem>
-          <MenuItem value="electric">Electric</MenuItem>
-        </TextField>
-        <TextField
-          style={{ display: 'block' }} fullWidth type="number"
-          label="Fuel Economy (L/100km, combined city/highway)"
-          value={this.state.fuel_economy || ''}
-          onChange={this.handleChange('fuel_economy')} />
-
+      <div style={{display: 'flex'}}>
+        <div style={{flex: 1}}>
+          <h2>Step 1: Your Current Vehicle</h2>
+          <VehicleSearch onChange={this.selectVehicle} />
+          <TextField
+            style={{ display: 'block' }} fullWidth
+            label="Vehicle Description"
+            value={currentVehicle.description || ''}
+            onChange={this.handleChange('description')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="Kerb Weight (kg)"
+            value={currentVehicle.kerbWeight || ''}
+            onChange={this.handleChange('kerbWeight')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth select
+            label="Fuel Type" value={currentVehicle.fuelType || ''}
+            onChange={this.handleChange('fuelType')}>
+            <MenuItem value="petrol">Petrol</MenuItem>
+            <MenuItem value="diesel">Diesel</MenuItem>
+            <MenuItem value="electric">Electric</MenuItem>
+          </TextField>
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label={currentVehicle.fuelType == "electric" ? "Energy usage (kW/100km, combined city/highway)" :
+              "Fuel Economy (L/100km, combined city/highway)"}
+            value={currentVehicle.fuelEconomy || ''}
+            onChange={this.handleChange('fuelEconomy')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="Total estimated lifespan (from new)"
+            value={currentVehicle.lifespan || ''}
+            onChange={this.handleChange('lifespan')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="Current value"
+            value={currentVehicle.value || ''}
+            onChange={this.handleChange('value')} />
+        </div>
+        <div style={{flex: 1}}>
+          <h2>Step 2: Your Life</h2>
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="How many KM do you drive each year?"
+            value={''}
+            onChange={this.handleChange('annualDistance')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="What is your interest rate (on the savings or debt that would be affected)?"
+            value={''}
+            onChange={this.handleChange('interestRate')} />
+        </div>
+        <div style={{flex: 1}}>
+          <h2>Step 3: Your New Vehicle</h2>
+          <VehicleSearch onChange={this.selectVehicle} />
+          <TextField
+            style={{ display: 'block' }} fullWidth
+            label="Vehicle Description"
+            value={currentVehicle.description || ''}
+            onChange={this.handleChange('description')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="Kerb Weight (kg)"
+            value={currentVehicle.kerbWeight || ''}
+            onChange={this.handleChange('kerbWeight')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth select
+            label="Fuel Type" value={currentVehicle.fuelType || ''}
+            onChange={this.handleChange('fuelType')}>
+            <MenuItem value="petrol">Petrol</MenuItem>
+            <MenuItem value="diesel">Diesel</MenuItem>
+            <MenuItem value="electric">Electric</MenuItem>
+          </TextField>
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="Fuel Economy (L/100km, combined city/highway)"
+            value={currentVehicle.fuelEconomy || ''}
+            onChange={this.handleChange('fuelEconomy')} />
+          <TextField
+            style={{ display: 'block' }} fullWidth type="number"
+            label="Purchase Price"
+            value={currentVehicle.value || ''}
+            onChange={this.handleChange('value')} />
+        </div>
       </div>
     );
   }
